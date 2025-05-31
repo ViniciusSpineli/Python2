0f844af30f562f19c1c2293bc3eeb43d9b7b87b0
VINICIUS SPINELI <vinicius.spineli@senacsp.edu.br>
2025-05-31 11:48:27 -0300

    Criação de models e migrations no app core

--- core/models.py
+++ core/models.py
@@ -4,9 +4,9 @@ from django.db import models
 class Cliente(models.Model):
     nome = models.CharField('Nome', max_length=100)
     sobrenome = models.CharField('Sobrenome', max_length=100)
-    sobrenome = models.EmailField('Email', max_length=100)
+    email = models.EmailField('Email', max_length=100)
     def __str__(self):
-        return f'{self.Nome} {self.sobrenome}'
+        return f'Nome: {self.nome} - Sobrenome: {self.sobrenome}'
     
 class Produto(models.Model):
     nome = models.CharField('Nome', max_length=100)
